98cd8c04547e6801afd479e93338f24c8ec2392e
Matteo C. <hello@micorksen.eu>
2025-01-31 21:16:09 +0100

    Update blesta_aad_plugin.php

--- blesta_aad_plugin.php
+++ blesta_aad_plugin.php
@@ -1,6 +1,29 @@
 <?php
+
+use Blesta\Core\Util\Common\Traits\Container;
+
+/**
+ * Microsoft Entra ID plugin handler
+ * @link https://speed-cloud.fr SPEED CLOUD
+ */
 class BlestaAADPlugin extends Plugin
 {
+    // Load traits
+    use Container;
+
+    /**
+     * @var Monolog\Logger An instance of the logger
+     */
+    protected $logger;
+
+    public function __construct()
+    {
+        $this->loadConfig(dirname(__FILE__) . DS . 'config.json');
+
+        // Initialize logger
+        $logger = $this->getFromContainer('logger');
+        $this->logger = $logger;
+    }
 
     /**
      * Listen to events.
