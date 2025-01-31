22ca97a4d7728c9c36bf3a31fcedabf1c33f3eef
Matteo C. <hello@micorksen.eu>
2025-01-31 21:11:19 +0100

    Create BlestaAAD.php

---
+++ BlestaAAD.php
@@ -0,0 +1,30 @@
+<?php
+class BlestaAAD extends Plugin
+{
+
+    /**
+     * Listen to events.
+     * @return array
+     */
+    public function getEvents()
+    {
+        return [
+            [
+                'event' => 'Appcontroller.structure',
+                'callback' => ['this', 'on_admin_login'],
+            ]
+        ];
+    }
+
+    /**
+     * Handles when an admin tries to access the admin page.
+     *
+     * @param array $event
+     * @return array
+     */
+    public function on_admin_login($event)
+    {
+        return ['head' => '<!-- This is just a test. -->'];
+    }
+    
+}
