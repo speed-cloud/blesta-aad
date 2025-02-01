78b768e4cdf06e302c6f4e2fe893ee7f5a1f45e1
Matteo C. <hello@micorksen.eu>
2025-02-01 14:19:49 +0100

    Create ms_entra_id_manage_plugin.php

---
+++ language/fr_fr/ms_entra_id_manage_plugin.php
@@ -0,0 +1,8 @@
+<?php
+$lang['MsEntraIdManagePlugin.!success.settings_updated'] = 'Les paramètres ont été mis à jour avec succès !';
+
+$lang['MsEntraIdManagePlugin.index.tenant_id'] = 'Identifiant du tenant ou nom de domaine du tenant';
+$lang['MsEntraIdManagePlugin.index.client_id'] = 'Identifiant du client';
+$lang['MsEntraIdManagePlugin.index.client_secret'] = 'Clé secrète du client';
+$lang['MsEntraIdManagePlugin.index.replace_admin_login_page'] = 'Remplacer la page de connexion des administrateurs';
+$lang['MsEntraIdManagePlugin.index.save'] = 'Sauvegarder';
