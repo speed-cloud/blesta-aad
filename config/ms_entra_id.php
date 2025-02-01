8859d3cbd7920e87221d637353e25f9581f385ef
Matteo C. <hello@micorksen.eu>
2025-02-01 13:49:38 +0100

    Delete config/ms_entra_id.php

--- config/ms_entra_id.php
+++
@@ -1,5 +0,0 @@
-<?php
-Configure::set('MsEntraIdPlugin.tenant_id', '');
-Configure::set('MsEntraIdPlugin.client_id', '');
-Configure::set('MsEntraIdPlugin.client_secret', '');
-Configure::set('MsEntraIdPlugin.force_sso', false);
