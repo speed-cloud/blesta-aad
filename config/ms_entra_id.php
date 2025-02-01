0d92a39d5bae0fd9a0d1118c837e3d5b4fef8258
Matteo C. <hello@micorksen.eu>
2025-02-01 13:42:29 +0100

    Create ms_entra_id.php

---
+++ config/ms_entra_id.php
@@ -0,0 +1,5 @@
+<?php
+Configure::set('MsEntraId.tenant_id', '');
+Configure::set('MsEntraId.client_id', '');
+Configure::set('MsEntraId.client_secret', '');
+Configure::set('MsEntraId.force_sso', false);
