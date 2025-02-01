b7e46df05db57229010c1a0f268e7ccbde73755b
Matteo C. <hello@micorksen.eu>
2025-02-01 16:06:35 +0100

    Create main.php

---
+++ controllers/main.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Microsoft Entra ID plugin handler
+ *
+ * @copyright Copyright (c) 2025, SPEED CLOUD
+ * @license GPL
+ * @link https://speed-cloud.fr SPEED CLOUD
+ */
+class Main extends AppController
+{
+    /**
+    * Handle login requests
+    */
+    public function login()
+    {
+        echo 'hey';
+        return;
+    }
+}
